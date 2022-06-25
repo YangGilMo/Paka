33d602d65bde394ced243a198f7884a7b327480d
YangGilMo <ygm119@naver.com>
2022-06-25 16:20:03 +0900

    java 1일차 commit

---
+++ Day01/src/MyFirstClass.java
@@ -0,0 +1,16 @@
+/*
+ * 2022년 6월25일(토)
+ * 만든이 : 양길모
+ * 제목 : My First Class
+ * 주석은 코딩을 어떻게 했는지에대하여 메모할때 사용
+ */
+
+
+public class MyFirstClass {
+
+	//프로그램 스타팅 포인트
+	public static void main(String[] args) {
+		System.out.println("MyFirstClass"); //My First Class를 출력한다.
+		System.out.println("만세만세"); //만세만세를 출력한다.
+	}
+}
---
+++ Day01/src/MyScondClass.java
@@ -0,0 +1,11 @@
+
+
+
+public class MyScondClass {// 자바 클래스의 시작
+ // 클래스 블록
+	public static void main(String[] args) { // 메서드 시작
+		//메서드 블록
+		//**..무장들	
+	} // 메서드 끝
+	
+} // 자바 클래스의 끝
