7685bad3054d4014a9f75c4758026d47ddc8ba65
YangGilMo <ygm119@naver.com>
2022-06-25 16:34:42 +0900

    커밋 테스트

--- Day01/src/MyScondClass.java
+++ Day01/src/MyScondClass.java
@@ -5,7 +5,8 @@ public class MyScondClass {// 자바 클래스의 시작
  // 클래스 블록
 	public static void main(String[] args) { // 메서드 시작
 		//메서드 블록
-		//**..무장들	
+		//**..문장들
+		System.out.println("Git Commit 후 변경해보기");
 	} // 메서드 끝
 	
 } // 자바 클래스의 끝
